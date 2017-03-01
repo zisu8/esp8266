33e62931a245eef47014f657bd589d3ad5b332c4
Martin Ommerle <martin.ommerle@siemens.com>
2017-03-01 10:48:57 +0100

    teilweise umbau auf DHT22 + bme280

--- lua/dht22_display.lua
+++ lua/dht22_display.lua
@@ -24,8 +24,6 @@ function init_OLED(sda,scl) --Set up the u8glib lib
    sla = 0x3C
    i2c.setup(0, sda, scl, i2c.SLOW)
    disp = u8g.ssd1306_128x64_i2c(sla)
-   disp:setFont(u8g.font_helvR14)
-
    disp:setFontRefHeightExtendedText()
    disp:setDefaultForegroundColor()
    disp:setFontPosTop()
@@ -36,23 +34,26 @@ function print_OLED()
  disp:firstPage()
  repeat
    --disp:drawFrame(2,2,126,62)
-   disp:drawStr(5, 10, str1)
-   disp:drawStr(5, 30, str2)
+
+   disp:setFont(u8g.font_6x10)
+   disp:drawStr(2, 10, "dht22")
+   disp:drawStr(2, 20, dht22_temp.." "..dht22_humi)
+--   disp:drawStr(5, 30, dht22_humi)
+--   disp.drawStr(5, 32, "bme280:")
+--   disp:drawStr(5, 42, bme280_temp)
+--   disp:drawStr(5, 52, bme280_humi)
    --disp:drawCircle(18, 47, 14)
  until disp:nextPage() == false
 end
 
-function read_clima_data()
-  status, temp, humi, temp_dec, humi_dec = dht.read(pin)
+function read_dht22_data()
+  local status, temp, humi, temp_dec, humi_dec = dht.read(pin)
   if status == dht.OK then
     -- Float firmware using this example
     --print("DHT Temperature:"..temp)
     --print("DHT Humidity:"..humi)
-    str1 = "Temp: "..temp.." °C"
-    str2 = "Humi: "..humi.." %"
-    print(str1)
-    print(str2)
-    print_OLED()
+    dht22_temp = "T: "..temp.." °C"
+    dht22_humi = "H: "..humi.." %"
   elseif status == dht.ERROR_CHECKSUM then
     print( "DHT Checksum error." )
   elseif status == dht.ERROR_TIMEOUT then
@@ -60,8 +61,23 @@ function read_clima_data()
   end
 end
 
+--function read_bme280_data()
+--   bme280.init(sda, scl)
+--   local temp, pressure, humi, qnh = bme280.read()
+--   if temp < 0 then
+--      bme280_temp = string.format("-%d.%02d", -temp/100, -_temp%100)
+--   else
+--      bme280_temp = string.format("%d.%02d", temp/100, temp%100)
+--   end
+--      bme280_humiy = "Humi: "..humi.." %"
+--      bme280_prese = "Pres: "..pressure.." HPa"
+--end
 -- Output Display
+
 init_OLED(sda,scl)
-read_clima_data()
+read_dht22_data()
+print ("DHT22 "..dht22_temp.." | "..dht22_humi)
+--read_bme280_data()
+print_OLED()
 print("Going to sleep for "..(sleep_time/1000/1000).." seconds...")
-node.dsleep(sleep_time)
+--node.dsleep(sleep_time)
